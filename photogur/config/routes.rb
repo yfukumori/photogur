6c59130fa210b21fc0cb37fd57f9e5480cc2ef9f
yfukumori <yuji.fukumori@gmail.com>
2014-06-18 16:12:17 -0400

    Adding feature to delete pictures

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -11,6 +11,9 @@ Photogur::Application.routes.draw do
 
   get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   patch 'pictures/:id' => "pictures#update"
+
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+  
   
 end
   # The priority is based upon order of creation: first created -> highest priority.
